1744d5c7f7153b697b6bdaabed421e4d8378a91e
a.sivtsov <https://gpub.globo-tech.com/>
2024-07-12 01:18:30 +0400

    fix pictures

--- src/components/TopComponent/index.tsx
+++ src/components/TopComponent/index.tsx
@@ -5,14 +5,14 @@ function TopComponent () {
     <div className='topComponentContainer'>
       <div className='navContainer'>
         <div className='navContainerSocial'>
-          <a href="https://web.telegram.org/k/"><img src="../assets/telegram.png" alt="telega" className='linkButton' /></a>
-          <a href="https://www.instagram.com/"><img src="../assets/instagram.png" alt="inst" className='linkButton'/></a>
-          <a href="https://x.com/"><img src="../assets/x.png" alt="x" className='linkButton'/></a>
+          <a href="https://web.telegram.org/k/"><img src="../../assets/telegram.png" alt="telega" className='linkButton' /></a>
+          <a href="https://www.instagram.com/"><img src="../../assets/instagram.png" alt="inst" className='linkButton'/></a>
+          <a href="https://x.com/"><img src="../../assets/x.png" alt="x" className='linkButton'/></a>
         </div>
         <button className='navContainerButton'>meng laboratory</button>
       </div>
 
-      <img src="src/assets/logo.png" alt="logo" className="logo" />
+      <img src="../assets/logo.png" alt="logo" className="logo" />
       <div className='textContainer'>
         <p className='text'>HI, I'M $MENG!</p>
         <p className='text'>INSPIRED BY AH MENG, HERE TO BRING SMILES AND SWAY INTO YOUR HEARTS!</p>
@@ -20,7 +20,7 @@ function TopComponent () {
       </div>
       <a href="#" className="navContainerButton joinButton">join community</a>
 
-      <img src="src/assets/meng.png" alt="meng" />
+      <img src="../assets/meng.png" alt="meng" />
     </div>
   )
 }
